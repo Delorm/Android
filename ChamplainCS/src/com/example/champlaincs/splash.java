a9a06ee9d895737c2e7bb2f1f1664742fcae7e67
nathan <nathanlacasse@hotmail.com>
2015-02-05 09:13:04 -0500

    save

--- ChamplainCS/src/com/example/champlaincs/splash.java
+++ ChamplainCS/src/com/example/champlaincs/splash.java
@@ -2,15 +2,31 @@ package com.example.champlaincs;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splash extends Activity {
-
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) 
-	{
-		super.onCreate(savedInstanceState);
+	public void onCreate(Bundle savedInstaceState){
+		
+		super.onCreate(savedInstaceState);
 		setContentView(R.layout.splash);
-
+		
+		int secondsDelayed=10000;
+		new Handler().postDelayed(new Runnable(){
+			
+			public void run(){
+				startActivity(new Intent(splash.this,MainActivity.class));
+				finish();
+			}
+			
+			
+		}, secondsDelayed);
+		
 	}
+	
+	
+	
 }
