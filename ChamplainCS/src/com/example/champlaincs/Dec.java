8d57162e81e0e0d34246175bf5be6e1f8699f7fb
nathan <nathanlacasse@hotmail.com>
2015-02-05 15:04:11 -0500

    the calm before the storm

--- ChamplainCS/src/com/example/champlaincs/Dec.java
+++ ChamplainCS/src/com/example/champlaincs/Dec.java
@@ -2,6 +2,7 @@ package com.example.champlaincs;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,7 +10,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 
 
@@ -22,6 +25,17 @@ public class Dec extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dec);
+		
+	String MOVIE_URL = "https://www.youtube.com/watch?v=wsmzqSJK7Nc";
+	
+	VideoView vid = (VideoView)findViewById(R.id.videoView1);
+	
+	Uri video = Uri.parse(MOVIE_URL);
+	vid.setMediaController(new MediaController(this));
+	vid.setVideoURI(video);
+	vid.start();
+	vid.requestFocus();
+		
 		
 		
 	ListView listView1 = (ListView) findViewById(R.id.listView1);
